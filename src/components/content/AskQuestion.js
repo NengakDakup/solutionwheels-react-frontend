59512e1af7220856d5ae9f3c429e0506994db701
nengak <nengakdakup@gmail.com>
2019-11-23 06:45:49 +0100

    i don\'t know what i did

--- src/components/content/AskQuestion.js
+++ src/components/content/AskQuestion.js
@@ -18,8 +18,14 @@ class AskQuestion extends Component{
         return (
             <div className="ask-question-outer">
                 <div className="ask-question-wrap">
+                    <div className="ask-question-top">
+                        <p>Ask Question</p>
+                        <p>Explain</p>
+                        <p>Add Image</p>
+                        <button onClick={ () => toggleDropDown('ask')} className="ask-question-x">Close</button>
+                    </div>
                     <p>Hellooooo!...</p>
-                    <button onClick={ () => toggleDropDown('ask')}>Close</button>
+                    
                 </div>
             </div>
         )
--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
-const server = "https://solutionwheels.herokuapp.com";
+const server = "http://localhost:5000";
+let temp = 'https://solutionwheels.herokuapp.com';
 
 
 export default server;
